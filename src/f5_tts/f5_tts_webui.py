7f0539a71f488b87fd05c977ba1ed31780418ce8
VYNCX <117465410+VYNCX@users.noreply.github.com>
2025-03-26 11:42:01 +0700

    Update f5_tts_webui.py

--- src/f5_tts/f5_tts_webui.py
+++ src/f5_tts/f5_tts_webui.py
@@ -21,6 +21,7 @@ from f5_tts.infer.utils_infer import (
 from f5_tts.model import DiT, UNetT
 from f5_tts.model.utils import seed_everything
 import torch
+from cleantext.number_tha import replace_numbers_with_thai
 
 #ถ้าอยากใช้โมเดลที่อัพเดทใหม หรือโมเดลภาษาอื่น สามารถแก้ไขโค้ด Model และ Vocab เช่น default_model_base = "hf://VIZINTZOR/F5-TTS-THAI/model_350000.pt"
 default_model_base = "hf://VIZINTZOR/F5-TTS-THAI/model_475000.pt"
@@ -77,11 +78,13 @@ def infer_tts(
     
     ref_audio, ref_text = preprocess_ref_audio_text(ref_audio_orig, ref_text)
     cross_fade_duration = float(cross_fade_duration)
-
+    
+    gen_text_cleaned = replace_numbers_with_thai(gen_text)
+    
     final_wave, final_sample_rate, combined_spectrogram = infer_process(
         ref_audio,
         ref_text,
-        gen_text,
+        gen_text_cleaned,
         f5tts_model,
         vocoder,
         cross_fade_duration=cross_fade_duration,
