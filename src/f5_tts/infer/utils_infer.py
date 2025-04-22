ef2b942b41ab5a35be71e111f22e0a88bb5b2acd
VYNCX <117465410+VYNCX@users.noreply.github.com>
2025-04-22 12:22:49 +0700

    Update utils_infer.py

--- src/f5_tts/infer/utils_infer.py
+++ src/f5_tts/infer/utils_infer.py
@@ -380,11 +380,12 @@ def infer_process(
     speed=speed,
     fix_duration=fix_duration,
     device=device,
+    set_max_chars=250
 ):
     # Split the input text into batches
     audio, sr = torchaudio.load(ref_audio)
-    max_chars = int(len(ref_text.encode("utf-8")) / (audio.shape[-1] / sr) * (25 - audio.shape[-1] / sr))
-    gen_text_batches = chunk_text(gen_text, max_chars=max_chars)
+    #max_chars = int(len(ref_text.encode("utf-8")) / (audio.shape[-1] / sr) * (25 - audio.shape[-1] / sr))
+    gen_text_batches = chunk_text(gen_text, max_chars=set_max_chars)
     for i, gen_text in enumerate(gen_text_batches):
         print(f"gen_text {i}", gen_text)
     print("\n")
